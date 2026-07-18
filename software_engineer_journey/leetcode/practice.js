d7641857f46cad7e7c1744137d6a69fa97325b49
NELSNEAKO <unggoykiat10@gmail.com>
2026-07-19 01:55:51 +0800

    added practice file to practice, today was no leetcode but learning agentic AI

---
+++ software_engineer_journey/leetcode/practice.js
@@ -0,0 +1,11 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+  return function (...args) {
+    console.log('Hello World');
+  };
+};
+
+const f = createHelloWorld();
+f({}, null, 42); // "Hello World"
